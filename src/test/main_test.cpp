7e5f8ab8eb89c449d4e563978882c2448e850801
Ayuba Tahiru <58880393+Tayuba@users.noreply.github.com>
2021-12-18 02:30:32 +0100

    Update main_test.cpp

--- src/test/main_test.cpp
+++ src/test/main_test.cpp
@@ -1,27 +1,15 @@
 //
 // Created by ludwig on 22.11.16.
-//
-
-
 #include "Tests.h"
-using namespace std;
 
-int main(int argc, char* argv[])
-{
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+namespace ClassProject{
+
+    TEST(uniqueTableSizeTest, ChecksSizeOfTable) {
+        Manager *m = new Manager();
+        EXPECT_EQ(m->uniqueTableSize(), 2);
+    }
+    int main(int argc, char *argv[]) {
+        ::testing::InitGoogleTest(&argc, argv);
+        return RUN_ALL_TESTS();
+    }
 }
-
-// #include "Tests.h"
-
-// namespace ClassProject{
-
-//     TEST(uniqueTableSizeTest, ChecksSizeOfTable) {
-//         Manager *m = new Manager();
-//         EXPECT_EQ(m->uniqueTableSize(), 0);
-//     }
-//     int main(int argc, char *argv[]) {
-//         ::testing::InitGoogleTest(&argc, argv);
-//         return RUN_ALL_TESTS();
-//     }
-// }

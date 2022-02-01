f7131b5f1bd4c09d6883cd0deb18fda0a277883f
Paulette <paula.iskander2@gmail.com>
2022-02-01 21:44:22 +0100

    Add reachability files

---
+++ src/reachability/Reachability.h
@@ -0,0 +1,18 @@
+//
+// Created by ludwig on 27.11.18
+// Refactored by deutschmann on 19.04.21
+//
+
+#ifndef VDSPROJECT_REACHABILITY_H
+#define VDSPROJECT_REACHABILITY_H
+
+#include "ReachabilityInterface.h"
+
+namespace ClassProject {
+
+//    class Reachability : public ReachabilityInterface {
+//
+//    };
+
+}
+#endif

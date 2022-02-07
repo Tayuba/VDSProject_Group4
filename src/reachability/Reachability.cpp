154768e85c0ecad7990623c0e4c861317c9fc73b
Paulette <paula.iskander2@gmail.com>
2022-02-07 15:00:47 +0100

    Implement setInitState function

--- src/reachability/Reachability.cpp
+++ src/reachability/Reachability.cpp
@@ -10,7 +10,21 @@ const std::vector<ClassProject::BDD_ID> &ClassProject::Reachability::getStates()
         return current_states;
 }
 
+void ClassProject::Reachability::setInitState(const std::vector<bool> &stateVector){
+    if(stateVector.size()!=initial_states.size()){
+        throw std::runtime_error("Size does not match with number of state bits");
+    }
+    else{
+        for(int i=0; i<stateVector.size();i++){
+            if(stateVector[i])
+                initial_states[i]=1;
+            else
+                initial_states[i]=0;
+        }
+    }
 
+
+}
 //void ClassProject::Reachability::setTransitionFunctions(const std::vector<BDD_ID> &transitionFunctions) {
 //
 //

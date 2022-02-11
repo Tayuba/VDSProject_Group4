9bab36320fc98808e39d336bdf5fceebf8eeb226
Nabih-Saleh <94145276+Nabih-Saleh@users.noreply.github.com>
2022-02-11 22:28:24 +0100

    Test cases added

--- src/reachability/main_test.cpp
+++ src/reachability/main_test.cpp
@@ -7,35 +7,41 @@
 
 namespace ClassProject {
 
-
-//    TEST(ReachabilityInterface_Constructor, Check_UniqueTableSize) {
-//        Reachability m(2);
-//        EXPECT_EQ(m.uniqueTableSize(), 6);
-//    }
-
-//    TEST(ReachabilityInterface_Constructor, Check_Initializations) {
-//        Reachability m(0);
-//        EXPECT_EQ(m.uniqueTableSize(), 2);
-//    }
-
-    TEST(ReachabilityInterface_Constructor, Check_Initializations) {
+    TEST(ReachabilityInterface_Constructor, Check_Unique_Table_Elements) {
         Reachability m(2);
-       // EXPECT_EQ(m.uniqueTableSize(), 6);
-        //unique table elements
         EXPECT_EQ(m.unique_table[2].label, "s0");
         EXPECT_EQ(m.unique_table[3].label, "s1");
         EXPECT_EQ(m.unique_table[4].label, "s'0");
         EXPECT_EQ(m.unique_table[5].label, "s'1");
-        //init states
+    }
+    TEST(ReachabilityInterface_Constructor, Check_Init_States) {
+        Reachability m(2);
         EXPECT_EQ(m.initial_states[0], 0);
         EXPECT_EQ(m.initial_states[1], 0);
-        //current states
+    }
+
+    TEST(ReachabilityInterface_Constructor, Check_Current_States) {
+        Reachability m(2);
         EXPECT_EQ(m.current_states[0], 2);
         EXPECT_EQ(m.current_states[1], 3);
-        //next states
+    }
+
+    TEST(ReachabilityInterface_Constructor, Check_Next_States) {
+        Reachability m(2);
         EXPECT_EQ(m.next_states[0], 4);
         EXPECT_EQ(m.next_states[1], 5);
     }
+
+    TEST(GetStates_return, Check_whole_states) {
+        Reachability m(5);
+        EXPECT_EQ(m.getStates()[0], 2);
+        EXPECT_EQ(m.getStates()[1], 3);
+        EXPECT_EQ(m.getStates()[2], 4);
+        EXPECT_EQ(m.getStates()[3], 5);
+        EXPECT_EQ(m.getStates()[4], 6);
+    }
+
+
 
 
 

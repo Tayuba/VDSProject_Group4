eed6c475dd2faa0889e18559bcedf87e43b7d639
Nabih-Saleh <94145276+Nabih-Saleh@users.noreply.github.com>
2022-02-12 21:46:44 +0100

    Test Coverage extended

--- src/reachability/main_test.cpp
+++ src/reachability/main_test.cpp
@@ -7,6 +7,10 @@
 
 namespace ClassProject {
 
+
+    TEST(ReachabilityInterface_Constructor, Check_RuntimeErr_0) {
+        EXPECT_ANY_THROW(Reachability m(0));
+    }
     TEST(ReachabilityInterface_Constructor, Check_Unique_Table_Elements) {
         Reachability m(2);
         EXPECT_EQ(m.unique_table[2].label, "s0");
@@ -40,9 +44,37 @@ namespace ClassProject {
         EXPECT_EQ(m.getStates()[3], 5);
         EXPECT_EQ(m.getStates()[4], 6);
     }
+    TEST(IsReachable, Check_RuntimeErr_DiffSize) {
+        Reachability m(2);
+        EXPECT_ANY_THROW(m.isReachable({0,0,1,1}));
 
+    }
+//    TEST(IsReachable, Check_reachability) {
+//        Reachability m(2);
+//
+//        EXPECT_EQ(m.isReachable({0,0}), true);
+//        EXPECT_EQ(m.isReachable({1,1}), true);
+//        EXPECT_EQ(m.isReachable({1,0}), false);
+//        EXPECT_EQ(m.isReachable({0,1}), false);
+//    }
 
+    TEST(SetInitState, Check_RuntimeErr_DiffSize) {
+        Reachability m(2);
+        std::vector<bool> myVec = {1,0,0,1,1};
+        EXPECT_ANY_THROW(m.setInitState(myVec));
+    }
 
+    TEST(SetInitState, Check_InitState) {
+        Reachability m(5);
+        std::vector<bool> myVec = {1, 0, 0, 1, 1};
+        m.setInitState(myVec);
+
+        EXPECT_TRUE(m.initial_states[0]);
+        EXPECT_FALSE(m.initial_states[1]);
+        EXPECT_FALSE(m.initial_states[2]);
+        EXPECT_TRUE(m.initial_states[3]);
+        EXPECT_TRUE(m.initial_states[4]);
+    }
 
 
     int main(int argc, char *argv[]) {

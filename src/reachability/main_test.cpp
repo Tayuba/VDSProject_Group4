4f938b1b1823df22638a818d21d621e5f549eec7
Nabih-Saleh <94145276+Nabih-Saleh@users.noreply.github.com>
2022-02-13 21:18:14 +0100

    More tests added

--- src/reachability/main_test.cpp
+++ src/reachability/main_test.cpp
@@ -7,11 +7,10 @@
 
 namespace ClassProject {
 
-    //CONSTRUCTOR TESTS
+    //================================================ CONSTRUCTOR TESTS ==================================
     TEST(ReachabilityInterface_Constructor, Check_RuntimeErr_0) {
         EXPECT_THROW(Reachability m(0), std::runtime_error);
     }
-
     TEST(ReachabilityInterface_Constructor, Check_Unique_Table_Elements) {
         Reachability m(2);
         EXPECT_EQ(m.unique_table[2].label, "s0");
@@ -19,7 +18,6 @@ namespace ClassProject {
         EXPECT_EQ(m.unique_table[4].label, "s'0");
         EXPECT_EQ(m.unique_table[5].label, "s'1");
     }
-
     TEST(ReachabilityInterface_Constructor, Check_default_TF){
         Reachability m(4);
         EXPECT_EQ(m.transition_functions[0], 2);
@@ -27,26 +25,23 @@ namespace ClassProject {
         EXPECT_EQ(m.transition_functions[2], 4);
         EXPECT_EQ(m.transition_functions[3], 5);
     }
-
     TEST(ReachabilityInterface_Constructor, Check_Init_States) {
         Reachability m(2);
         EXPECT_EQ(m.initial_states[0], 0);
         EXPECT_EQ(m.initial_states[1], 0);
     }
-
     TEST(ReachabilityInterface_Constructor, Check_Current_States) {
         Reachability m(2);
         EXPECT_EQ(m.current_states[0], 2);
         EXPECT_EQ(m.current_states[1], 3);
     }
-
     TEST(ReachabilityInterface_Constructor, Check_Next_States) {
         Reachability m(2);
         EXPECT_EQ(m.next_states[0], 4);
         EXPECT_EQ(m.next_states[1], 5);
     }
 
-    //&getStates() TESTS
+    //================================================ &getStates() TESTS ==================================
     TEST(GetStates_return, Check_whole_states) {
         Reachability m(5);
         EXPECT_EQ(m.getStates()[0], 2);
@@ -55,6 +50,8 @@ namespace ClassProject {
         EXPECT_EQ(m.getStates()[3], 5);
         EXPECT_EQ(m.getStates()[4], 6);
     }
+
+    //================================================ isReachable TESTS ==================================
     TEST(IsReachable, Check_RuntimeErr_DiffSize) {
         Reachability m(2);
         EXPECT_THROW(m.isReachable({0,0,1,1}), std::runtime_error);
@@ -69,14 +66,15 @@ namespace ClassProject {
 //        EXPECT_EQ(m.isReachable({0,1}), false);
 //    }
 
+    //================================================ setTransitionFunctions TESTS ==================================
     TEST(SetTransitionFunction, Check_RuntimeErr_DiffSize) {
         Reachability m(2);
-        EXPECT_THROW(m.setTransitionFunctions({0,0,1,1}), std::runtime_error);
+        EXPECT_THROW(m.setTransitionFunctions({0,5,1,3}), std::runtime_error);
     }
-//    TEST(SetTransitionFunction, Check_RuntimeErr_UnkwonID) {
-//        Reachability m(2);
-//        EXPECT_THROW(m.setTransitionFunctions({0,0,1,1}), std::runtime_error);
- //   }
+    TEST(SetTransitionFunction, Check_RuntimeErr_UnkwonID) {
+        Reachability m(3);
+        EXPECT_THROW(m.setTransitionFunctions({0,1,5000}), std::runtime_error);
+    }
     TEST(SetTransitionFunction, Check_provided_Vector) {
         Reachability m(4);
         m.setTransitionFunctions({1,2,3,4});
@@ -86,11 +84,11 @@ namespace ClassProject {
         EXPECT_EQ(m.transition_functions[3],4);
     }
 
+    //================================================ setInitState TESTS ==================================
     TEST(SetInitState, Check_RuntimeErr_DiffSize) {
         Reachability m(2);
         EXPECT_THROW(m.setInitState({0,0,1,1}), std::runtime_error);
     }
-
     TEST(SetInitState, Check_InitState) {
         Reachability m(5);
         m.setInitState({1, 0, 0, 1, 1});
